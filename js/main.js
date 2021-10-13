fe480d52134c400199bd5e8b253a34ced44e2d02
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2021-10-13 22:29:23 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-alert ("surf-board");
+var modalOpen = document.getElementById(".hamburger"),
+ modal = document.getElementById(".active"),
+ modalclose = document.querySelector(".active__close");
+
+ modalOpen.onclick = function() {
+     modal.style.dysplay="block";
+ };
+
+ modalclose.onclick = function () {
+    modal.style.dysplay="none";
+
+ }
+
+ window.onclick = function (event) {
+     if(event.target == modal) {
+        modal.style.dysplay="none";         
+     }
+ }
