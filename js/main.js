ae409f0d04bf43b1f209bafd5ab2091d45cc5db9
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2021-10-14 18:24:30 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-var modalOpen = document.getElementById(".hamburger"),
- modal = document.getElementById(".active"),
- modalClose = document.querySelector(".active__close");
+ const modal = document.querySelector(".active"),
+ clickModal = document.querySelector(".hamburger"),
+ closeModak = document.querySelector(".active__close")
+ console.log(modal);
 
- modalOpen.onclick = function() {
-     modal.style.dysplay="block";
- };
+ clickModal.onclick = function() {
+    modal.style.dysplay="block";
+};
 
- modalClose.onclick = function () {
-    modal.style.dysplay="none";
 
- }
-
- window.onclick = function (event) {
-     if(event.target == modal) {
-        modal.style.dysplay="none";         
-     }
- }
+window.onclick = function (event) {
+    if(event.target == modal) {
+       modal.style.dysplay="none";         
+    }
+}
