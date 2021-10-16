9c87f8440c98ecb753703339b0e5fa0d338a624a
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2021-10-16 22:17:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const clickModal = document.querySelector(".hamburger"),
 modal = document.querySelector(".active"),
-closeModal = document.querySelector(".active__close");
+closeModal = document.querySelector(".active__close"),
+heroClose = document.querySelector(".wrapper");
 
 
 clickModal.onclick = function() {
@@ -13,8 +14,15 @@ closeModal.onclick = function () {
 
 }
 
-window.onclick = function (e) {
-   if(e.target == modal) {
-       modal.style.display="none";        
-   }
+const clickMenu = document.querySelector(".options__svg"),
+openMenu = document.querySelector(".emerge");
+
+
+clickMenu.onmouseover = function () {
+    openMenu.style.display="block";
 }
+
+openMenu.onmouseout = function () {
+    openMenu.style.display="none";
+}
+
