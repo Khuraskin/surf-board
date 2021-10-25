e88cbad80942473de3a72debfad9e282102467e7
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2021-10-25 13:42:41 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,7 @@ const clickModal = document.querySelector(".hamburger"),
 modal = document.querySelector(".active"),
 closeModal = document.querySelector(".active__close"),
 heroClose = document.querySelector(".wrapper"),
-closeLink = document.querySelector(".active__link"),
-closeSurf = document.querySelector(".active__surf"),
-closeCommand = document.querySelector(".active__command"),
-closeCarte = document.querySelector(".active__carte"),
-closeReviews = document.querySelector(".active__reviews"),
-closeWork = document.querySelector(".active__work"),
-closeСontacts = document.querySelector(".active__contacts");
+closeLink = document.querySelectorAll(".active__link");
 
 clickModal.onclick = function() {
    modal.style.display = "block";
@@ -19,39 +13,12 @@ closeModal.onclick = function () {
    modal.style.display = "none";
 
 }
+closeLink.forEach(function(element){
+   element.addEventListener('click' , closeMenu);
+ })
 
-closeLink.onclick = function () {
-   modal.style.display = "none";
-   console.log(closeLink);
-}
 
-closeSurf.onclick = function () {
+ function closeMenu(){
    modal.style.display = "none";
-   console.log(closeLink);
-}
-
-closeCommand.onclick = function () {
-   modal.style.display = "none";
-   console.log(closeLink);
-}
-
-closeCarte.onclick = function () {
-   modal.style.display = "none";
-   console.log(closeLink);
-}
-
-closeReviews.onclick = function () {
-   modal.style.display = "none";
-   console.log(closeLink);
-}
-
-closeWork.onclick = function () {
-   modal.style.display = "none";
-   console.log(closeLink);
-}
-
-closeСontacts.onclick = function () {
-   modal.style.display = "none";
-   console.log(closeLink);
-}
+ }
 
