4cffb0debdb36e8cef268838a57d9fa23fa7c309
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2021-10-26 16:25:46 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
 const clickModal = document.querySelector(".hamburger"),
-modal = document.querySelector(".active"),
-closeModal = document.querySelector(".active__close"),
-heroClose = document.querySelector(".wrapper"),
-closeLink = document.querySelectorAll(".active__link");
+  modal = document.querySelector(".active"),
+  closeModal = document.querySelector(".active__close"),
+  heroClose = document.querySelector(".wrapper"),
+  closeLink = document.querySelectorAll(".active__link");
 
-clickModal.onclick = function() {
-   modal.style.display = "block";
-   console.log(clickModal);
-}
+clickModal.onclick = function () {
+  modal.style.display = "block";
+  console.log(clickModal);
+};
 
 closeModal.onclick = function () {
-   modal.style.display = "none";
+  modal.style.display = "none";
+};
 
+closeLink.forEach(function (element) {
+  element.addEventListener("click", closeMenu);
+});
+
+function closeMenu() {
+  modal.style.display = "none";
 }
-closeLink.forEach(function(element){
-   element.addEventListener('click' , closeMenu);
- })
+
+const findBlockByAlias = alias => {
+  return $(".comment__main").filter((ndx, main) =>{
+    return $(main).attr("data-connect") == alias
+  }) 
+}
+
+$(".clients__link").click((e) => {
+  e.preventDefault();
 
 
- function closeMenu(){
-   modal.style.display = "none";
- }
-
+  const $this = $(e.currentTarget);
+  const target = $this.attr("data-open");
+  const mainToShow = findBlockByAlias(target);
+  const curItem = $this.closest(".clients__info");
+  mainToShow.addClass("comment__main--active").siblings().removeClass("comment__main--active");
+  curItem.addClass("clients__info--active").siblings().removeClass("clients__info--active");
+});
